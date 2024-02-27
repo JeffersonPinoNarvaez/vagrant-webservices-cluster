66aeaeb8f1425a3afc5131f5dc3e60263e90fe88
jefferson  pino <jefferson.pino@lulosoft.com>
2024-02-26 21:05:50 -0500

    structure was modified to be cleaner. webApp provision file was fixed to use only one for all web servers.

--- provisions/web-balancer/balancerServer.sh
+++ provisions/web-balancer/balancerServer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+ip_address=$(hostname -I | awk '{print $2}')
+echo "La dirección IP de la máquina virtual es: $ip_address"
 
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install haproxy -y
 sudo systemctl enable haproxy
 
-sudo cp /home/vagrant/sharedFolder/haproxy.cfg /etc/haproxy/haproxy.cfg
+
+sudo rsync -avz -e ssh /vagrant/configurations/haproxy.cfg vagrant@$ip_address:/etc/haproxy/haproxy
 
 sudo systemctl restart haproxy
